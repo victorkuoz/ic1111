3a7722667f9dc232ef86846669e5c6d367c715ba
victorkuoz <b08902069@csie.ntu.edu.tw>
2022-12-03 22:38:04 +0800

    query 5

--- mySQL/queries/5.sql
+++ mySQL/queries/5.sql
@@ -1,1 +1,14 @@
 -- 5. Which planet(s) have not been visited by any characters in all movies?
+
+SELECT
+	Name
+FROM
+	Planets
+WHERE	
+	Name NOT IN (
+		SELECT
+			DISTINCT Planet_Name
+		FROM
+			TimeTable
+	);
+
