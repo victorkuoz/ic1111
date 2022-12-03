87b52c2a1dceb76ade6ffdb6d204c971abc85950
victorkuoz <b08902069@csie.ntu.edu.tw>
2022-12-04 00:03:27 +0800

    query 3 & 4

--- mySQL/queries/3.sql
+++ mySQL/queries/3.sql
@@ -1,2 +1,13 @@
 -- 3. Show the characters names and the total time they spent in each planet among the movies.
 
+SELECT
+	Tmp.Character_Name, Tmp.Planet_Name, SUM(Tmp.Time) AS TotalTime 
+FROM (
+	SELECT
+		Character_Name, Planet_Name, (Time_of_Arrival + Time_of_Departure) AS Time
+	FROM
+		TimeTable) AS Tmp
+GROUP BY
+	Character_Name ,Planet_Name
+ORDER BY
+	Character_Name, Planet_Name;
--- mySQL/queries/4.sql
+++ mySQL/queries/4.sql
@@ -1,1 +1,4 @@
 -- 4. Find distinct names of the planets visited by rebels-affiliated human.
+
+SELECT DISTINCT Planet_Name FROM TimeTable Where Character_Name IN (
+	SELECT Name FROM Characters WHERE Race='Human' AND Affiliation='rebels');
