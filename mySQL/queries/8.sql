b430d82daae5a6058ce14c5122f0a5bf014a4fc7
victorkuoz <b08902069@csie.ntu.edu.tw>
2022-12-05 01:08:05 +0800

    query 8

--- mySQL/queries/8.sql
+++ mySQL/queries/8.sql
@@ -1,1 +1,45 @@
--- 8. For Luke Skywalker, for each movie that Luke appears in, what is the planet that has the different affiliation with him and that he travels to for the longest length of time? (movie, planet)
+-- 8. For Luke Skywalker, for each movie that Luke appears in, 
+    -- what is the planet that has the different affiliation with him and that he travels to for the longest length of time? (movie, planet)
+
+SELECT
+    Cur.Planet_Name, Cur.Time, Cur.Movie
+FROM
+    (
+        SELECT
+            T.Planet_Name, T.Movie, (T.Time_of_Departure - T.Time_of_Arrival) AS `Time`
+        FROM
+            TimeTable T
+        WHERE
+            T.Character_Name = 'Luke Skywalker' AND T.Planet_Name NOT IN (
+                SELECT 
+                    P.Name
+                FROM
+                    Planets P
+                WHERE
+                    P.Affiliation = (SELECT C.Affiliation FROM Characters C WHERE C.Name = 'Luke Skywalker')
+            )
+    ) AS Cur
+WHERE
+    Cur.Time = 
+    (
+        SELECT
+            MAX(Prev.Time)
+        FROM
+            (
+                SELECT
+                    T.Planet_Name, T.Movie, (T.Time_of_Departure - T.Time_of_Arrival) AS `Time`
+                FROM
+                    TimeTable T
+                WHERE
+                    T.Character_Name = 'Luke Skywalker' AND T.Planet_Name NOT IN (
+                        SELECT 
+                            P.Name
+                        FROM
+                            Planets P
+                        WHERE
+                            P.Affiliation = (SELECT C.Affiliation FROM Characters C WHERE C.Name = 'Luke Skywalker')
+                    )
+            ) AS Prev
+        WHERE
+            Cur.Movie = Prev.Movie
+    )
