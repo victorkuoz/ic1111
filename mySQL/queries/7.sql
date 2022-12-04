5d7aa5cde5cc499a98c5862d8189b127d31ff2ee
victorkuoz <b08902069@csie.ntu.edu.tw>
2022-12-05 00:27:21 +0800

    query 7

--- mySQL/queries/7.sql
+++ mySQL/queries/7.sql
@@ -1,1 +1,16 @@
 -- 7. How many times for each character to visit his/her homeworld? show character names and times appeared in TimeTable.
+
+SELECT
+    C.Name,
+    (
+        SELECT
+            COUNT(*)
+        FROM
+            TimeTable T
+        WHERE
+            C.Name = T.Character_Name AND C.Homeworld = T.Planet_Name
+    ) AS `Count`
+FROM
+    Characters C
+ORDER BY
+    C.Name
