5ac13154c11b83ed80c75f5b463a7a3dedcc30b1
victorkuoz <b08902069@csie.ntu.edu.tw>
2022-12-06 01:52:45 +0800

    query 9 - comment unnecessary code

--- mySQL/queries/9.sql
+++ mySQL/queries/9.sql
@@ -1,29 +1,25 @@
 -- 9. For each race, find the character that travels the most (number of time slots) in each movie. (movie, race, character_name)
 
--- SELECT T.Character_name, T.Movie, T.Time_of_Departure - T.Time_of_Arrival AS `Time` FROM TimeTable T;
+-- SELECT
+-- 	T.Character_name, T.Movie, SUM(T.Time) AS `Time`
+-- FROM
+-- 	(SELECT T.Character_name, T.Movie, (T.Time_of_Departure - T.Time_of_Arrival) AS `Time` FROM TimeTable T) AS T
+-- GROUP BY
+-- 	T.Character_name, T.Movie;
 
-SELECT * FROM Characters;
-
-SELECT
-	T.Character_name, T.Movie, SUM(T.Time) AS `Time`
-FROM
-	(SELECT T.Character_name, T.Movie, (T.Time_of_Departure - T.Time_of_Arrival) AS `Time` FROM TimeTable T) AS T
-GROUP BY
-	T.Character_name, T.Movie;
-
-SELECT
-	T_.Movie, MAX(T_.Time)
-FROM
-	(SELECT
-		T.Character_name, T.Movie, SUM(T.Time) AS `Time`
-	FROM
-		(SELECT T.Character_name, T.Movie, (T.Time_of_Departure - T.Time_of_Arrival) AS `Time` FROM TimeTable T) AS T
-	GROUP BY
-		T.Character_name, T.Movie) T_
-WHERE
-	T_.Character_name IN (SELECT C_.Name FROM Characters C_ WHERE C_.Race = 'Human')
-GROUP BY
-	T_.Movie;
+-- SELECT
+-- 	T_.Movie, MAX(T_.Time)
+-- FROM
+-- 	(SELECT
+-- 		T.Character_name, T.Movie, SUM(T.Time) AS `Time`
+-- 	FROM
+-- 		(SELECT T.Character_name, T.Movie, (T.Time_of_Departure - T.Time_of_Arrival) AS `Time` FROM TimeTable T) AS T
+-- 	GROUP BY
+-- 		T.Character_name, T.Movie) T_
+-- WHERE
+-- 	T_.Character_name IN (SELECT C_.Name FROM Characters C_ WHERE C_.Race = 'Human')
+-- GROUP BY
+-- 	T_.Movie;
 
 
 SELECT
