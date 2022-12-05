45121a8f262465ff845e585f8d1713622d27fd24
victorkuoz <b08902069@csie.ntu.edu.tw>
2022-12-06 02:04:12 +0800

    add alias

--- mySQL/queries/5.sql
+++ mySQL/queries/5.sql
@@ -1,14 +1,13 @@
 -- 5. Which planet(s) have not been visited by any characters in all movies?
 
 SELECT
-	Name
+	P.Name
 FROM
-	Planets
+	Planets P
 WHERE	
-	Name NOT IN (
+	P.Name NOT IN (
 		SELECT
-			DISTINCT Planet_Name
+			DISTINCT T.Planet_Name
 		FROM
-			TimeTable
+			TimeTable T
 	);
-
