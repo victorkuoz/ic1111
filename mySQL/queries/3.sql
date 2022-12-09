d87f0a3a488d015c978da94e8a0e6e5c66a8d746
victorkuoz <b08902069@csie.ntu.edu.tw>
2022-12-09 18:37:10 +0800

    query 3 - remove unnecessary code

--- mySQL/queries/3.sql
+++ mySQL/queries/3.sql
@@ -1,13 +1,8 @@
 -- 3. Show the characters names and the total time they spent in each planet among the movies.
 
 SELECT
-	Tmp.Character_Name, Tmp.Planet_Name, SUM(Tmp.Time) AS TotalTime 
-FROM (
-	SELECT
-		Character_Name, Planet_Name, (Time_of_Arrival + Time_of_Departure) AS Time
-	FROM
-		TimeTable) AS Tmp
+	T.Character_Name, T.Planet_Name, SUM(T.Time_of_Departure - T.Time_of_Arrival) AS TotalTime 
+FROM 
+	TimeTable T
 GROUP BY
-	Character_Name ,Planet_Name
-ORDER BY
-	Character_Name, Planet_Name;
+	T.Character_Name, T.Planet_Name;
