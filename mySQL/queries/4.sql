2e405e91d56c5e4501487cabe57c30a508b0fde8
victorkuoz <b08902069@csie.ntu.edu.tw>
2022-12-09 18:40:00 +0800

    query 4 - add alias

--- mySQL/queries/4.sql
+++ mySQL/queries/4.sql
@@ -1,4 +1,4 @@
 -- 4. Find distinct names of the planets visited by rebels-affiliated human.
 
-SELECT DISTINCT Planet_Name FROM TimeTable Where Character_Name IN (
-	SELECT Name FROM Characters WHERE Race='Human' AND Affiliation='rebels');
+SELECT DISTINCT T.Planet_Name FROM TimeTable T Where T.Character_Name IN (
+	SELECT C.Name FROM Characters C WHERE C.Race='Human' AND C.Affiliation='rebels');
